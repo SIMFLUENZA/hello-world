e100f16b10aebc517ff48f17da27318f2bf74e02
SIMFLUENZA <54793979+SIMFLUENZA@users.noreply.github.com>
2019-09-03 00:39:42 +0900

    Update forExample.java

--- Basic2/src/if_else/forExample.java
+++ Basic2/src/if_else/forExample.java
@@ -8,7 +8,7 @@ public class forExample {
 		//          반복적으로 실행된 구문
 			for(int i =0;  i<10; i++) {
 				System.out.println("Everything is everywhere" + i);
-			//HEY WHERE ARE YOU GOINGdsad
+			// now 아임 계쏙해서수정중
 			}
 	  }
 }
